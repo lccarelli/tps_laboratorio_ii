a5323950e59e8eb68986c2fe2d9289ffd6324324
Lau Carelli <carelli.lc@gmail.com>
2022-04-27 11:21:32 -0300

    se vuelve a subir

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        #region Atributos
+
+        private const char MAS = '+';
+        private const char MENOS = '-';
+        private const char MULTIPLICAR = '*';
+        private const char DIVIDIR = '/';
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Valida y realiza la operacion requerida entre ambos numeros ingresados.
+        /// </summary>
+        /// <param name="num1">Primer numero de tipo Operando utilizado para la operacion</param>
+        /// <param name="num2">Segundo numero de tipo Operando utilizado para la operacion</param>
+        /// <param name="operador"> Operador a ser utilizado en el calculo </param>
+        /// <returns></returns>
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            double resultado = 0;
+
+            switch (ValidarOperador(operador))
+            {
+                case MAS:
+                    resultado = num1 + num2;
+                    break;
+                case MENOS:
+                    resultado = num1 - num2;
+                    break;
+                case MULTIPLICAR:
+                    resultado = num1 * num2;
+                    break;
+                case DIVIDIR:
+                    resultado = num1 / num2;
+                    break;
+            }
+            return resultado;
+        }
+
+        /// <summary>
+        ///  Valida si el operador ingreado se encuentra dentro de una lista de operadores disponibles.
+        /// </summary>
+        /// <param name="operador">de tipo char operador seleccionado.</param>
+        /// <returns>Operador seleccionado si lo encuentra, si no devuelve operador de suma</returns>
+        private static char ValidarOperador(char operador)
+        {
+            if (operador == MAS || operador == MENOS || operador == MULTIPLICAR || operador == DIVIDIR)
+            {
+                return operador;
+            }
+            return MAS;
+        }
+
+        #endregion
+    }
+}
---
+++ TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        #region Atributos
+
+        private List<string> listOperadores = new() { "+", "-", "*", "/" };
+        private const string MESSAGE_INVALID_VALUE = "Valor inválido";
+
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        ///  Constructor clase FormCalculadora. Inicializa 
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            lblResultado.Text = string.Empty;
+
+            foreach (string operador in listOperadores)
+            {
+                this.cmbOperador.Items.Add(operador);
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        ///  Boton de cerrar aplicación.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        ///  Boton convierte un numero de decimal a binario.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Operando.DecimalBinario(lblResultado.Text);
+            MostrarOperaciones($"Convertido a binario -> {lblResultado.Text}");
+        }
+
+        /// <summary>
+        ///  Boton convierte de binario a decimal.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Operando.BinarioDecimal(lblResultado.Text);
+            MostrarOperaciones($"Convertido a decimal -> {lblResultado.Text}");
+        }
+
+        /// <summary>
+        ///  Boton limpia los textBox de la pantalla.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        /// <summary>
+        ///  Metodo llamado para limpiar objetos del formulario.
+        /// </summary>
+        private void Limpiar() 
+        {
+            this.txtNumero1.Text = string.Empty;
+            this.txtNumero2.Text = string.Empty;
+            this.lblResultado.Text = string.Empty;
+            this.cmbOperador.Text = string.Empty;
+        }
+
+        /// <summary>
+        ///  Boton de operar llama al metodo Operar Calculo.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            string operadorSeleccionado = (string)cmbOperador.SelectedItem;
+            string result = Operar(txtNumero1.Text, txtNumero2.Text, operadorSeleccionado).ToString();
+
+            if (result != double.MinValue.ToString())
+            {
+                lblResultado.Text = result;
+            }else 
+            {
+                lblResultado.Text = MESSAGE_INVALID_VALUE;
+            };
+        
+            MostrarOperaciones($"{txtNumero1.Text} {operadorSeleccionado} {txtNumero2.Text} = {lblResultado.Text}");
+        }
+
+        /// <summary>
+        ///  Metodo que opera calculo pasado por el boton Operar.
+        /// </summary>
+        /// <param name="numero1">String contiene numero operando 1.</param>
+        /// <param name="numero2">String contiene numero operando 2.</param>
+        /// <param name="operador">string contiene el operador del calculo.</param>
+        private static double Operar(string numero1, string numero2, string operador) 
+        {
+            char operadorChar;
+
+            if (operador is null)
+            {
+                operadorChar = '+';
+            }
+            else 
+            {
+                operadorChar = char.Parse(operador);
+            }
+
+            return Calculadora.Operar(new Operando(numero1), new Operando(numero2), operadorChar);
+        }
+
+        /// <summary>
+        ///  Metodo que muestra las operaciones en el listBox.
+        /// </summary>
+        /// <param name="operacion">String con la operacion completa.</param>
+        private void MostrarOperaciones(string operacion) 
+        {
+            lstOperaciones.Items.Add(operacion);
+        }
+
+        /// <summary>
+        ///  Metodo que limpia pantalla cuando se termina de iniciar el formulario y antes de que se dibuje.
+        /// </summary>
+        /// <param name="sender">Instancia del boton.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        /// <summary>
+        /// Metodo que abre una ventada y pregunta si se quiere salir de la aplicacion.
+        /// </summary>
+        /// <returns>
+        /// DialogResult -> DialogResult.Yes || Dialog.Result.NO
+        /// </returns>
+        private DialogResult QuererSalir() 
+        {
+            return MessageBox.Show("¿Está seguro que desea salir?","Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question);       
+        }
+
+        /// <summary>
+        ///  Metodo que cierra el formulario desde la 'x'.
+        /// </summary>
+        /// <param name="sender">Instancia del form.</param>
+        /// <param name="e">Informacion del evento.</param>
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.QuererSalir() == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+        }
+        #endregion
+    }
+}
