ae193fb7beda3c6a35a8768cab90e896a8dec34f
lccarelli <carelli.lc@gmail.com>
2022-04-29 22:51:23 -0300

    se sube tp2

---
+++ TP-02/Entidades/Sedan.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        /// <summary>
+        /// Atributos de la clase.
+        /// </summary>
+        ETipo tipo;
+
+        /// <summary>
+        /// Propiedad sobreescrita de la clase heredada retornará el tamaño del Sedan
+        /// Return ETamanio -> Mediano
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            :this(marca, chasis, color, ETipo.CuatroPuertas)
+        { 
+        }
+
+        /// <summary>
+        /// Constructor de clase que asigna chasis, marca y color a la clase vehiculo, y tipo a la clase actual.
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+            : base(chasis, marca, color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Devuelve todos los datos del Sedan en formato string
+        /// </summary>
+        /// <returns>Datos del automóvil</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", Tamanio);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Tipos disponibles para Sedan
+        /// </summary>
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+    }
+}
---
+++ TP-02/Entidades/Taller.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Taller
+    {
+        /// <summary>
+        /// Atributos de la clase.
+        /// </summary>
+        private List<Vehiculo> vehiculos;
+        private int espacioDisponible;
+
+        #region "Constructores"
+        /// <summary>
+        /// Constructor privado que inicializa la lista de vehículos.
+        /// </summary>
+        private Taller()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+
+        /// <summary>
+        /// Constructor público que inicializa espacio disponible.
+        /// privado sin parametros.
+        /// </summary>
+        /// <param name="espacioDisponible"></param>
+        public Taller(int espacioDisponible) 
+            : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro TODOS los vehículos dentro del taller.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Taller.Listar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="taller">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public static string Listar(Taller taller, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", taller.vehiculos.Count, taller.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Vehiculo v in taller.vehiculos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Ciclomotor:
+                        if(v is Ciclomotor) sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.Sedan:
+                        if(v is Sedan) sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.SUV:
+                        if(v is Suv) sb.AppendLine(v.Mostrar());
+                        break;
+                    default:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista validando que haya espacio disponible en el taller.
+        /// </summary>
+        /// <param name="taller">Objeto donde se agregará el elemento</param>
+        /// <param name="vehiculo">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Taller operator +(Taller taller, Vehiculo vehiculo)
+        {
+            if (taller.vehiculos.Count < taller.espacioDisponible) 
+            {
+                foreach (Vehiculo v in taller.vehiculos)
+                {
+                    if (v == vehiculo)
+                        return taller;
+                }
+
+                taller.vehiculos.Add(vehiculo);
+            }
+            return taller;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista validando que este se encuentre en ella.
+        /// </summary>
+        /// <param name="taller">Objeto donde se quitará el elemento</param>
+        /// <param name="vehiculo">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Taller operator -(Taller taller, Vehiculo vehiculo)
+        {
+            foreach (Vehiculo v in taller.vehiculos)
+            {
+                if (v == vehiculo)
+                {
+                    taller.vehiculos.Remove(v);
+                    break;
+                }
+            }
+
+            return taller;
+        }
+        #endregion
+
+        /// <summary>
+        /// Enumerado de tipos posibles de vehiculos.
+        /// </summary>
+        public enum ETipo
+        {
+            Ciclomotor, Sedan, SUV, Todos
+        }
+    }
+}
